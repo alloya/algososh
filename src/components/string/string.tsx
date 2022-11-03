902dca97c423917397aa1079989d7afc18b05776
Yulia Lushinskaya <crimson-alloya@yandex.ru>
2022-11-03 16:45:17 +0400

    Update string.tsx

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,33 +1,45 @@
 import React, { useEffect, useState } from "react";
+import { ElementStates } from "../../types/element-states";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
 import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import s from "./string.module.css";
 
+type TCircle = {
+  char: string,
+  style: ElementStates
+}
+
 export const StringComponent: React.FC = () => {
   const [input, setInput] = useState("");
-  const [word, setWord] = useState<string[]>([]);
+  const [word, setWord] = useState<TCircle[]>([]);
   const [loader, setLoader] = useState(false);
-  const [nextReverse, setnNextReverse] = useState<string[]>([]);
-  const [start, setStart] = useState(0);
-  const [end, setEnd] = useState(0);
-  //let newOrder:string[] = [];
+  const [nextReverse, setnNextReverse] = useState<TCircle[]>([]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInput(event.target.value)
   }
 
   useEffect(() => {
-    setnNextReverse(input.split(""));
+    setnNextReverse(input.split("").map(el => ({ char: el, style: ElementStates.Default })));
   }, [input])
 
   useEffect(() => {
     let interval: number | null = null;
+    let colorInterval: number | null = null;
     if (loader) {
       let start = 0;
       let end = nextReverse.length - 1;
-      let newOrder = [...nextReverse];
+      let newOrder = nextReverse.map(el => el);
+      colorInterval = window.setInterval(() => {
+        newOrder = colorElements(newOrder, start, end)
+        setWord(newOrder);
+        if (start + 1 > end) {
+          clearInterval(colorInterval!)
+          setLoader(false);
+        }
+      }, 500)
       interval = window.setInterval(() => {
         if (start < end) {
           newOrder = switchFunc(newOrder, start, end)
@@ -36,7 +48,6 @@ export const StringComponent: React.FC = () => {
           end--;
         }
         if (start >= end) {
-          setLoader(false);
           clearInterval(interval!);
         }
       }, 1000)
@@ -47,13 +58,31 @@ export const StringComponent: React.FC = () => {
   }, [loader])
 
   const handleClick = async () => {
-    setWord(nextReverse);
-    setEnd(nextReverse.length - 1);
-    setStart(0);
+    setnNextReverse(nextReverse.map(el => ({ char: el.char, style: ElementStates.Default })))
+    const elementsArray: TCircle[] = nextReverse.map(el => ({ char: el.char, style: ElementStates.Default }));
+    setWord(elementsArray);
     setLoader(true);
   }
 
-  const switchFunc = (array: string[], firstIndex: number, secondIndex: number) => {
+  const colorElements = (array: TCircle[], firstIndex: number, secondIndex: number): TCircle[] => {
+    let newArray: TCircle[] = [...array];
+    if (firstIndex !== 0 && secondIndex !== array.length - 1) {
+      if (array[firstIndex - 1].style == ElementStates.Changing) {
+        array[firstIndex - 1].style = ElementStates.Modified;
+        array[secondIndex + 1].style = ElementStates.Modified;
+      }
+    }
+    if (array[firstIndex].style == ElementStates.Default) {
+      array[firstIndex].style = ElementStates.Changing;
+      array[secondIndex].style = ElementStates.Changing;
+    }
+    if (firstIndex === secondIndex) {
+      array[firstIndex].style = ElementStates.Modified;
+    }
+    return [...newArray]
+  }
+
+  const switchFunc = (array: TCircle[], firstIndex: number, secondIndex: number) => {
     const temp = array[firstIndex];
     array[firstIndex] = array[secondIndex];
     array[secondIndex] = temp;
@@ -67,7 +96,7 @@ export const StringComponent: React.FC = () => {
         <Button text={"Развернуть"} onClick={handleClick} isLoader={loader} />
       </div>
       <div className={`d-flex justify-content-center ${s.charContainner}`}>
-        {word.map((char, index) => <Circle letter={char} key={index} extraClass={"pr-6"} />)}
+        {word.map((el, index) => <Circle letter={el.char} state={el.style} key={index} extraClass={"pr-6"} />)}
       </div>
     </SolutionLayout>
   );
