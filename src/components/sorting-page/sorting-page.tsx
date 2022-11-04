c3fb43d552bb86236cad6e39fc1798f24f7e5b52
Yulia Lushinskaya <crimson-alloya@yandex.ru>
2022-11-04 23:43:08 +0400

    bubble

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Direction } from "../../types/direction";
 import { ElementStates } from "../../types/element-states";
 import { delay, getRndInteger, switchFunc } from "../../utils/utils";
@@ -22,21 +22,51 @@ export const SortingPage: React.FC = () => {
   const [array, setArray] = useState<TColumn[]>([]);
 
   const handleClick = async (sortType: Direction) => {
-    sortType === Direction.Ascending ? setAscLoader(true) : setAscLoader(true)
-    await selectSort(sortType);
-    setAscLoader(false)
+    sortType === Direction.Ascending ? setAscLoader(true) : setDescLoader(true)
+    if (select) {
+      await selectSort(sortType)
+    }
+    if (bubble) {
+      await bubbleSort(sortType)
+    }
+    //select ? await selectSort(sortType) : await bubbleSort(sortType);
+    sortType === Direction.Ascending ? setAscLoader(false) : setDescLoader(false);
   }
 
   const generateArray = () => {
-    const len = getRndInteger(3, 17);
+    const len = getRndInteger(3, 7);
     let array = [];
     for (let i = 0; i < len; i++) {
       array.push(getRndInteger(1, 100));
     }
     array = array.map(el => ({ num: el, style: ElementStates.Default }))
     setArray(array);
-    console.log(array);
+  }
 
+  const bubbleSort = async (sortType: Direction) => {
+    let temp = [...array];
+    let end = temp.length - 1;
+    for (let i = 0; i < temp.length; i++) {
+      for (let j = 0; j < temp.length - i - 1; j++) {
+        temp[j].style = ElementStates.Changing;
+        if (j+1 <= end) { temp[j + 1].style = ElementStates.Changing; }
+        if (j > 0) { temp[j - 1].style = ElementStates.Default; }
+
+        setArray([...temp]);
+        await delay(500)
+        if (temp[j].num > temp[j+1].num) {
+          let newA = switchFunc(temp, j, j + 1);
+          setArray(newA);
+          await delay(500)
+        }
+      }
+      if (end-1 > 0) {
+        temp[end-1].style = ElementStates.Default;
+      }
+      temp[end].style = ElementStates.Modified;
+      setArray([...temp]);
+      end--;
+    }
   }
 
   const selectSort = async (sortType: Direction) => {
@@ -46,20 +76,20 @@ export const SortingPage: React.FC = () => {
     while (start < array.length) {
       for (let i = start + 1; i < array.length; i++) {
         if (i >= 2) {
-          temp[i-1].style = ElementStates.Default;
+          temp[i - 1].style = ElementStates.Default;
         }
         temp[indexToSwitch].style = ElementStates.Changing;
         temp[i].style = ElementStates.Changing;
         setArray([...temp]);
         await delay(DELAY);
-        if (sortType === Direction.Ascending ? (temp[i].num < temp[indexToSwitch].num) :  (temp[i].num > temp[indexToSwitch].num)) {
+        if (sortType === Direction.Ascending ? (temp[i].num < temp[indexToSwitch].num) : (temp[i].num > temp[indexToSwitch].num)) {
           temp[indexToSwitch].style = ElementStates.Default;
           indexToSwitch = i
         }
       }
       temp[indexToSwitch].style = ElementStates.Modified;
-      if (indexToSwitch != array.length-1) {
-        temp[array.length-1].style = ElementStates.Default;
+      if (indexToSwitch != array.length - 1) {
+        temp[array.length - 1].style = ElementStates.Default;
       }
       setArray(switchFunc(temp, indexToSwitch, start));
       start++;
@@ -87,23 +117,25 @@ export const SortingPage: React.FC = () => {
             sorting={Direction.Ascending}
             onClick={() => handleClick(Direction.Ascending)}
             isLoader={ascLoader}
-            extraClass={"mr-6"} />
+            extraClass={"mr-6"}
+            disabled={descLoader} />
           <Button
             text={"По убыванию"}
             sorting={Direction.Descending}
             onClick={() => handleClick(Direction.Descending)}
-            isLoader={descLoader} />
+            isLoader={descLoader}
+            disabled={ascLoader} />
         </div>
         <Button
           text={"Новый массив"}
-          onClick={generateArray} />
+          onClick={generateArray}
+          disabled={ascLoader || descLoader} />
       </div>
       <div className="d-flex justify-content-center align-items-end">
         {array && array.map((el, index) => {
           return <Column index={el.num} key={index} extraClass={"pr-5"} state={el.style} />
         })}
       </div>
-
     </SolutionLayout>
   );
 };
